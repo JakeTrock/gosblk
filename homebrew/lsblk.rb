ce265fe7e9e49674563797beb8944ff3150dccdf
Jacob Trock <jgh@geocrypt.me>
2024-11-03 02:57:38 -0500

    desc

--- homebrew/lsblk.rb
+++ homebrew/lsblk.rb
@@ -1,6 +1,6 @@
 # lsblk.rb
 class lsblk < Formula
-  desc "Description of your lsblk tool"
+  desc "LSBLK for macos implemented in golang"
   homepage "https://github.com/JakeTrock/gosblk"
   url "https://github.com/JakeTrock/gosblk/archive/refs/tags/v1.tar.gz"
   sha256 "be6116ae1c07cd0b794b189a537aae302c52aa0257e2211551c8e6d2bd8a8a55"
